e535b942e6bcdd33c3fa5b29a66a92af8b6979a9
AntarahCo <slava3032001@gmail.com>
2024-02-15 13:02:56 +0600

    alert, modal design

--- JS/main.js
+++ JS/main.js
@@ -9,18 +9,47 @@ const ageInp = document.querySelector("#age");
 const passwordInp = document.querySelector("#password");
 const passwordConfirmInp = document.querySelector("#passwordConfirm");
 const registerForm = document.querySelector("#registerUser-form");
+const registerCancel = document.querySelector(".modal button[type='reset']");
 
 // modal logic
 
 function showModal() {
-  if (modal.style.display === "block") {
-    modal.style.display = "none";
-  } else {
+  if (!modal.classList.contains("show")) {
     modal.style.display = "block";
+    setTimeout(function () {
+      modal.classList.add("show");
+    }, 10);
+  } else {
+    modal.classList.remove("show");
   }
+}
+function hideModal() {
+  modal.classList.remove("show");
 }
 
 registerUserModalBtn.addEventListener("click", showModal);
+
+function isDescendant(parent, child) {
+  let node = child.parentNode;
+  while (node != null) {
+    if (node === parent) {
+      return true;
+    }
+    node = node.parentNode;
+  }
+  return false;
+}
+
+function clickOutsideModal(event) {
+  if (
+    !isDescendant(modal, event.target) &&
+    event.target !== registerUserModalBtn
+  ) {
+    modal.classList.remove("show");
+  }
+}
+
+document.addEventListener("click", clickOutsideModal);
 
 // register logic
 const USERS_API = "http://localhost:8000/users";
@@ -34,15 +63,19 @@ function registerUser(e) {
     !passwordInp.value.trim() ||
     !passwordConfirmInp.value.trim()
   ) {
-    alert("Some inputs are empty");
+    showMessage("Some inputs are empty");
     return;
   }
 
   if (passwordInp.value !== passwordConfirmInp.value) {
-    alert("Passwords are not correct");
+    showMessage("Passwords are not correct");
     return;
   }
 
+  if (/\d/.test(userNameInp.value)) {
+    showMessage("Username should not contain numbers");
+    return;
+  }
   const userObj = {
     username: userNameInp.value,
     email: emailInp.value,
@@ -64,9 +97,25 @@ function registerUser(e) {
   passwordInp.value = "";
   passwordConfirmInp.value = "";
 
-  alert("Registered successfully!");
+  showMessage("Registered successfully!");
 
+  modal.style.opacity = "0";
   modal.style.display = "none";
 }
 
 registerForm.addEventListener("submit", registerUser);
+registerCancel.addEventListener("click", hideModal);
+
+// message box logic
+
+const messageBox = document.querySelector(".messageBox");
+
+function showMessage(message) {
+  messageBox.textContent = message;
+  messageBox.classList.add("show");
+  setTimeout(hideMessage, 2000);
+}
+
+function hideMessage() {
+  messageBox.classList.remove("show");
+}
